e7ba5cbadf3f89c8fbe29230481968b4f6970be1
Greg Johns <dumbbros@gmail.com>
2023-12-20 13:09:29 -0500

    Added new features, better documentation, and a requirments file

--- BatchRename.py
+++ BatchRename.py
@@ -18,14 +18,14 @@ def extract_metadata(media_file):
         #general_stream = next((stream for stream in probe_data.get("streams", []) if stream.get("@type") == "General"), {})
 
         # Extract specific data from the video stream
-        video_duration = video_stream.get("duration", "N/A")
-        video_codec = video_stream.get("codec_name", "N/A")
-        width = video_stream.get("width", "N/A")
-        height = video_stream.get("height", "N/A")
+        video_duration = video_stream.get("duration", "N-A")
+        video_codec = video_stream.get("codec_name", "N-A")
+        width = video_stream.get("width", "N-A")
+        height = video_stream.get("height", "N-A")
 
         # Extract creation time from the tags
         tags = video_stream.get("tags", {})
-        encoded_date = tags.get("creation_time", "N/A")
+        encoded_date = tags.get("creation_time", "N-A")
 
         # Access information about the format (container)
         format_info = probe_data.get("format", {})
@@ -35,7 +35,7 @@ def extract_metadata(media_file):
         # Extract focal length from the format tags
         format_tags = format_info.get("tags", {})
         #VERY IMPORTANT - use. instead of _ for the format meta data. Use Media info app in HTML view for the proper names.
-        focal_length = format_tags.get("com.blackmagic-design.camera.lensFocalLength", "N/A")
+        focal_length = format_tags.get("com.blackmagic-design.camera.lensFocalLength", "N-A")
 
         # Create a dictionary with extracted data
         metadata = {
@@ -64,7 +64,7 @@ def classify_shot(focal_length):
         else:
             return "CU"
     except ValueError:
-        return "N/A"  # If focal length is not a valid number
+        return "N-A"  # If focal length is not a valid number
 
 # Function for determining if the shot was in the AM or PM.   USING UTC,  May need to adjust this to detect time zone?
 def classify_time_of_day(date_string):
@@ -83,7 +83,7 @@ def classify_time_of_day(date_string):
         else:
             return "NT" #NT for night???  sure.
     except ValueError:
-        return "N/A"  # If date format is not as expected
+        return "N-A"  # If date format is not as expected
 
 #--------------------------------- MAIN LOGIC -----------------------------
 @click.command(help="This script renames video files based on their metadata.")
@@ -92,8 +92,9 @@ def classify_time_of_day(date_string):
 @click.option('-l','--log', is_flag=True, help='Enable log file output.')
 @click.option('-r','--recursive', is_flag=True, help='Enable recursive file walk.')
 @click.option('-i', '--input', type=click.Path(exists=True), help='Input directory to process.')
+@click.option('-s','--sub', is_flag=True, help='Enable sub folder naming.')
 
-def main(debug,log,recursive,input):
+def main(debug,log,recursive,input,sub):
     wordmark= r"""
   ____        _       _       _____                                      
  |  _ \      | |     | |     |  __ \                                     
@@ -161,9 +162,9 @@ def main(debug,log,recursive,input):
                         # Check if the file has metadata
                         if metadata: #Only proceeds if we have meta data
                             # Extract relevant information
-                            shot_class = classify_shot(metadata.get("Focal Length", "N/A"))
-                            date = metadata.get("Date", "N/A")[:10] #Grabs the first 10 characters of the date string
-                            date_string = metadata.get("Date", "N/A") # Assuming your date string is stored in the metadata dictionary
+                            shot_class = classify_shot(metadata.get("Focal Length", "N-A"))
+                            date = metadata.get("Date", "N-A")[:10] #Grabs the first 10 characters of the date string
+                            date_string = metadata.get("Date", "N-A") # Assuming your date string is stored in the metadata dictionary
                             time_of_day_classification = classify_time_of_day(date_string)
                             #project_name = "OTW23" #THIS needs to be user input
 
@@ -172,8 +173,31 @@ def main(debug,log,recursive,input):
                             shot_numberSTR = str(shot_number_int).zfill(3)
                             #---- Increment shot number ----
 
+                            #---- Sub folder naming -----
+                            if sub:
+                                # Get the relative path from the input directory to the current file
+                                relative_path = os.path.relpath(file_path, input_directory)
+
+                                # Extract individual components from the relative path
+                                path_components = relative_path.split(os.path.sep)[:-1]
+                                click.echo(f"{click.style('Path components: ', fg='yellow')}{path_components}")
+
+                                # Use the extracted components in the new file name
+                                num_components = len(path_components)
+                                click.echo(f"{click.style('Num components: ', fg='yellow')}{num_components}")
+                                sub_path = "_".join(path_components[-num_components:],)
+
+                                #Only add the trailing underscore if the file is in a sub directory
+                                if num_components > 0:
+                                    sub_path += "_"
+                                
+                                click.echo(f"{click.style('Sub path: ', fg='yellow')}{sub_path}")
+                            else:
+                                sub_path = ""
+                            #---- Sub folder naming -----
+
                             # Generate the new file name
-                            new_file_name = f"{date}_{project_name}_{shot_class}_{time_of_day_classification}_{shot_numberSTR}{file_extension}"
+                            new_file_name = f"{date}_{project_name}_{sub_path}{shot_class}_{time_of_day_classification}_{shot_numberSTR}{file_extension}"
                             click.echo(f"{click.style('New file name: ', fg='green')}{new_file_name}")
 
                             #Print a responsive seperation line
--- README.md
+++ README.md
@@ -4,13 +4,14 @@
 
 This Python script renames video files based on their metadata. It utilizes FFmpeg to extract metadata information from video files.
 
-File format is as follows: {date}-{project}-{time}-{shot_class}-{shot_number}.{extension}
+File format is as follows: {date}_{project}_{sub-folder-path}_{time}_{shot_class}_{shot_number}.{extension}
 
 ## Features
 
 - Rename video files based on metadata information.
 - Customize file naming using project name, shot class, time of day, etc.
 - Enable debugging, log file output, and recursive file walk options.
+- Enable sub folder naming to maintain pre existing file organization.
 
 ## Requirements
 
@@ -19,10 +20,16 @@ File format is as follows: {date}-{project}-{time}-{shot_class}-{shot_number}.{e
 - FFmpeg-python (pip install ffmpeg-python)
 - Click (pip install click)
 
+Also feel free to use the requirements.txt file.  Preferably in a Python virtual environment.
+
+```bash
+pip install -r requirements.txt
+```
+
 ## Usage
 
 ```bash
-python BatchRename.py [-d] [-l] [-r] [-i INPUT_DIRECTORY]
+python BatchRename.py [-s] [-d] [-l] [-r] [-i INPUT_DIRECTORY]
 ```
 
 The following options are available:
@@ -30,6 +37,7 @@ The following options are available:
     -d or --debug: Enable debugging output.
     -l or --log: Enable log file output.
     -r or --recursive: Enable recursive file walk.
+    -s or --sub: Enable sub folder naming.
     -i INPUT_DIRECTORY or --input INPUT_DIRECTORY: Specify the input directory to process.
 
 ## Executable
@@ -37,5 +45,10 @@ The following options are available:
 The executable can be run from the terminal as well
 
 ```bash
-BatchRename.exe [-d] [-l] [-r] [-i INPUT_DIRECTORY]
+BatchRename.exe [-s] [-d] [-l] [-r] [-i INPUT_DIRECTORY]
 ```
+
+## Workflow
+
+For the best results you should organize your raw footage into a sensible folder hierarchy before running this script.
+In this case you would run with the -r and -s flags to search through and maintain that folder structure.  Also important to note that in this configuration you would want to run at the root of the hierarchy.
